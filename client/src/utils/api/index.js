a3c63e5a8ee42800daa8bfd91f8ce4992a94a18b
yagoFogaca <yagofogaca20@gmail.com>
2023-06-02 19:43:17 -0300

    feat: implementação da chamada da api

---
+++ client/src/utils/api/index.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://valorant-api.com/v1/agents';
+export class Api {
+  static async getAllAgents() {
+    return (await axios.get('?language=pt-BR')).data;
+  }
+}
