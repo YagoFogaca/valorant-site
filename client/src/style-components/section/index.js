a0921db0ef2e1c1991a18aa7aa0daf290dd16b7d
yagoFogaca <yagofogaca20@gmail.com>
2023-06-03 10:01:06 -0300

    style: direção do container

--- client/src/style-components/section/index.js
+++ client/src/style-components/section/index.js
@@ -2,8 +2,9 @@ import styled from 'styled-components';
 
 export const Section = styled.section`
   display: flex;
+  flex-direction: column;
+
   width: 100%;
   min-height: 25vh;
-
   padding: 25px 35px;
 `;
