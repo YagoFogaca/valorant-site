72d4d1df9a0048b01fbd137f4ab7b8934386a54a
yagoFogaca <yagofogaca20@gmail.com>
2023-06-03 10:18:40 -0300

    feat: aplicação da responsividade e position para botões do carrossel

--- client/src/style-components/section/index.js
+++ client/src/style-components/section/index.js
@@ -7,4 +7,9 @@ export const Section = styled.section`
   width: 100%;
   min-height: 25vh;
   padding: 25px 35px;
+  position: relative;
+
+  @media (max-width: 345px) {
+    padding: 25px 15px;
+  }
 `;
